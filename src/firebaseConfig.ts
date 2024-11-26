4056c8111d02696baf9f18ad0fc9f5a664a00e71
Swaggypanties <kausejere@gmail.com>
2024-11-26 10:38:31 +0200

    Fixed

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -30,7 +30,7 @@ const db = getFirestore()
 
 // Collection reference
 
-const colRef = collection(db)
+//const colRef = collection(db)
 
 // Function to log in the user
 export async function loginUser(username: string, password: string): Promise<boolean> {
